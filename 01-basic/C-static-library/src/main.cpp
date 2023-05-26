ffcc6992b297a40134f5cb6570edea3a606fc4a9
zkzk5214 <59314355+zkzk5214@users.noreply.github.com>
2023-05-26 14:42:18 +0800

    20230526

--- 01-basic/C-static-library/src/main.cpp
+++ 01-basic/C-static-library/src/main.cpp
@@ -1,5 +1,9 @@
 #include "static/Hello.h"
-
+/*
+Using this method means that there is less chance of header 
+    filename clashes when you use multiple libraries in 
+    your project.
+*/
 int main(int argc, char *argv[])
 {
     Hello hi;
